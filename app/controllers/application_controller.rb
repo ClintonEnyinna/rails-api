8f669744a629a6decb77cfa9a112cdee1d3594b0
Clinton Enyinna <enyinnaclinton@gmail.com>
2021-09-12 20:38:07 -0500

    authorize_request before action logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::API
   DEFAULT_LIMIT = 20
 
+  before_action :authenticate_request
+
+  attr_reader :current_user
+
+  private
+
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.new(request.headers).call
+
+    render json: { error: 'Not authorized' }, status: :unauthorized unless @current_user
+  end
+
   def default_limit
     params[:limit].presence || DEFAULT_LIMIT
   end
