66a0589bf08ee62ccf980c1d5a840b4ac880df06
Clinton Enyinna <enyinnaclinton@gmail.com>
2021-09-12 21:18:37 -0500

    create seed database

--- Gemfile
+++ Gemfile
@@ -43,3 +43,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'jsonapi-rails'
 gem 'jwt'
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+2.times do |i|
+  User.create(email: "#{Faker::Internet.email}", password: "foobar", password_confirmation: "foobar")
+end
+
+8.times do |i|
+  Task.create(title: "#{Faker::Food.dish}", note: "#{Faker::Food.description}", user: User.first)
+end
+
+2.times do |i|
+  Task.create(title: "#{Faker::Food.dish}", note: "#{Faker::Food.description}", user: User.last)
+end
